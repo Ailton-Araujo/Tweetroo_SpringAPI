55e854ca8e89c9ca5ce4d492ab979810706ffda2
Ailton Araujo <ailton.correia.araujo@gmail.com>
2024-01-26 23:26:54 -0300

    feat: /user route

--- src/main/java/com/tweteroo/api/ApiApplication.java
+++ src/main/java/com/tweteroo/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.tweteroo.api.api;
+package com.tweteroo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/tweteroo/api/dtos/UserDTO.java
@@ -0,0 +1,21 @@
+package com.tweteroo.api.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+
+public class UserDTO {
+
+    private static final String URL_PATTERN = "^https?://\\S+$";
+
+    @NotBlank(message = "Avatar must not be blank")
+    @Pattern(regexp = URL_PATTERN, message = "Avatar should in the format of a URL")
+    private String avatar;
+
+    @NotBlank(message = "Username must not be blank")
+    @Size(max = 100, message = "UserName could not be more than one hundred characters long")
+    private String username;
+}
